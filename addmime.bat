ccfdaae3e690eea09f1873ac088d301bc9da3638
Trevor Jones <jones1876@gmail.com>
2012-11-16 18:04:14 -0500

    add

---
+++ addmime.bat
@@ -0,0 +1,4 @@
+"%ProgramFiles%\IIs Express\appcmd" set config /section:staticContent /+[fileExtension='.webm',mimeType='video/webm']
+"%ProgramFiles%\IIs Express\appcmd" set config /section:staticContent /+[fileExtension='.ogv',mimeType='video/ogv']
+"%ProgramFiles%\IIs Express\appcmd" set config /section:staticContent /+[fileExtension='.mp4',mimeType='video/mp4']
+"%ProgramFiles%\IIs Express\appcmd" set config /section:staticContent /+[fileExtension='.m4v',mimeType='video/m4v']
